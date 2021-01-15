a1a974755f098a03de84bdc714bdfea5fd23d307
sneakstarberry <gihong2012@likeliion.org>
2021-01-15 10:48:02 +0900

    commit

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('', (req, res, next) => {
-  res.send(200)
-})
+app.get("", (req, res, next) => {
+  res.send("hello jenkins");
+});
 
 app.listen(80, () => {
-  console.log('listening...')
-})
+  console.log("listening...");
+});
